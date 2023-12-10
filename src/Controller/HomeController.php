ecb758740dc2e03f298423f01d05dc9e3fa9a43a
sadbrain <riotgheptrannhulol@gmail.com>
2023-12-09 16:52:13 -0800

    init Universe

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        
+        return $this -> render("home/index.html.twig");   
+
+    }
+    /**
+     * @Route("/custom/{name}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        // dump($request);
+        // dump($request -> get("name"));
+        $name = $request -> get("name");
+        return $this -> render("home/custom.html.twig",[
+            'name' => $name
+        ]);   
+    }
+}
